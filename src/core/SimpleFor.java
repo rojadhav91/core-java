aa888b74d339affdeb550cd500047522609f5b7b
rohit <rohit@rohit-HP-Pavilion-g6-Notebook-PC>
2018-05-17 04:00:28 +0530

    added a simple for loop programme

---
+++ src/core/SimpleFor.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SimpleFor {
+
+	public static void main(String[] args) throws Exception{
+		//WPA to print any table
+		System.out.println("enter any no");
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int a=Integer.parseInt(br.readLine());
+		for(int i=1;i<=10;i++){
+			int res=a*i;
+			System.out.println(res);
+		}
+
+	}
+
+}
